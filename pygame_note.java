cf2ed6241847531c7ba37496c87e4c7dbd97d591
zha34 <zha34@sfu.ca>
2018-10-09 15:51:47 -0700

    add more tut sites

--- pygame_note.java
+++ pygame_note.java
@@ -4,5 +4,19 @@
 
 // examples
 https://github.com/pygame/pygame/tree/master/examples
+
+
 // tut
+https://dr0id.bitbucket.io/legacy/pygame_tutorials.html
+
+
 http://programarcadegames.com/?chapter=example_code
+
+http://pygametutorials.wikidot.com/tutorials-basic
+
+http://thepythongamebook.com/en:pygame:start
+
+
+
+// run example:
+python3 -m pygame.examples.aliens
